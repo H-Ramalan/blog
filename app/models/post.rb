4b636feb670fa89976dd311e94bd019f910243f0
H-Ramalan <talktoramalan@gmail.com>
2023-09-28 11:22:55 -0700

    set up associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  #Relationship
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, class_name: 'Comment'
+  has_many :likes, class_name: 'Like'
 end
