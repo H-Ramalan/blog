906d8900fb50ce7372961c25c020de7716ff30ad
H-Ramalan <talktoramalan@gmail.com>
2023-09-28 13:53:22 -0700

    Insert data and created custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ApplicationRecord
   #Relationship
-    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  
+  def update_comment_count
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,13 @@ class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment'
   has_many :likes, class_name: 'Like'
+
+  def recent_comments
+    comments.order(created_at: :asc).limit(5)
+  end
+
+  def update_post_count
+    user.update(post_counter: user.posts.count)
+  end
+  4
 end
