fe196cdc68a89f4335143b08d3e56c2bb8e47d72
H-Ramalan <talktoramalan@gmail.com>
2023-09-28 13:55:09 -0700

    fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-  #Relationship
+  # Relationship
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
-  
+
   def update_comment_count
     post.update(comments_counter: post.comments.count)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  #Relationship
+  # Relationship
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
