c5005b7c425a7110399dc303d1db0e2196ca4e37
H-Ramalan <talktoramalan@gmail.com>
2023-10-03 23:21:16 -0700

    Added specs for controllers testing

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /users/:user_id/posts' do
+    it 'returns a correct response status' do
+      get '/users/:user_id/posts'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      get '/users/:user_id/posts'
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in response body' do
+      get '/users/:user_id/posts'
+      expect(response.body).to include('Here is the list of all posts')
+    end
+  end
+
+  describe 'GET / users/:user_id/posts/:id' do
+    it 'renders the show template' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to render_template(:show)
+    end
+
+    it 'returns a correct response status' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct placeholder text in response body' do
+      get '/users/:user_id/posts/:id'
+      expect(response.body).to include('Here are post for every user')
+    end
   end
 end
