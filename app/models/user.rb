dfa51b50fa0ed76d783d73cc002ba8d2e1f3f94a
RileyManda <rileymanda69@gmail.com>
2023-10-13 13:21:53 +0200

    Implemented validation on post counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'user_id', dependent: :destroy
   has_many :likes, class_name: 'Like', foreign_key: 'user_id', dependent: :destroy
 
+  after_initialize :set_default_post_counter
+
   def recent_post
     posts.order(created_at: :desc).limit(3)
   end
@@ -15,4 +17,10 @@ class User < ApplicationRecord
   # VALIDATIONS
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  private
+
+  def set_default_post_counter
+    self.post_counter ||= 0
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_181610) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "post_counter", null: true
+    t.integer "post_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_181610) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
